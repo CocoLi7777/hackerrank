758b18a7f5414389f399518d25d5a12c96f27b74
Coco <coco.li7777@gmail.com>
2020-05-14 15:34:10 +1000

    migratorybirds

---
+++ MigratoryBirds.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+  inputString = inputString.split('\n');
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+// Complete the migratoryBirds function below.
+function migratoryBirds(arr) {
+  let map = {};
+  let max = 0;
+  let arrTemp = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!map[arr[i]]) {
+      map[arr[i]] = 1;
+    } else {
+      map[arr[i]]++;
+      if (map[arr[i]] > max) {
+        max = map[arr[i]];
+        arrTemp.push(arr[i]);
+      }
+    }
+  }
+  if (arrTemp.length > 1) arrTemp.sort((a, b) => a - b);
+  return arrTemp[0];
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const arrCount = parseInt(readLine().trim(), 10);
+
+  const arr = readLine()
+    .replace(/\s+$/g, '')
+    .split(' ')
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  const result = migratoryBirds(arr);
+
+  ws.write(result + '\n');
+
+  ws.end();
+}
